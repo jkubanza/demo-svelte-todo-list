9557fc64066c49acece5923e06c894006ac997c6
Joel KUBANZA <jku@bpragmatic.fr>
2023-07-15 14:07:29 +0100

    restructured svg

---
+++ src/svg/DeleteIcon.svelte
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<svg
+    class="x-button-icon"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+>
+    <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M6 18L18 6M6 6l12 12"
+    />
+</svg>
